af839f01e8febb3daa74bd6981381ed4d963c8e6
Ray Plante <raymond.plante@nist.gov>
2019-01-08 12:12:48 -0500

    first component and app adapted for the LPS (missed modified files)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,20 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
 
-import {AppComponent} from './app.component';
-import {HomeComponent} from './home/home.component';
-import {TransferHttpCacheModule} from '@nguniversal/common';
+import { TransferHttpCacheModule } from '@nguniversal/common';
+import { AppComponent } from './app.component';
+import { FrameModule } from './frame/frame.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-  ],
-  imports: [
-    BrowserModule.withServerTransition({appId: 'my-app'}),
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full'},
-      { path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule'},
-      { path: 'lazy/nested', loadChildren: './lazy/lazy.module#LazyModule'}
-    ]),
-    TransferHttpCacheModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule.withServerTransition({appId: 'my-app'}),
+        TransferHttpCacheModule,
+        FrameModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }

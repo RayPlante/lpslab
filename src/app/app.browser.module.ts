a986ecfa80dfa089f75edfc4338f7c191432de26
Ray Plante <raymond.plante@nist.gov>
2019-01-30 10:25:17 -0500

    enable browser animations

--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { BrowserMetadataTransferModule } from './nerdm/metadatatransfer-browser.
 @NgModule({
     imports: [
         BrowserModule.withServerTransition({appId: 'PDR-LandingPageService'}),
+        BrowserAnimationsModule,
         AppModule,
         BrowserTransferStateModule,
         BrowserMetadataTransferModule
